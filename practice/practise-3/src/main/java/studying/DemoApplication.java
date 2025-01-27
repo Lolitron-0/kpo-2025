29d1137db696d81c55131a927e25113cca05df44
Lolitron-0 <ovcharov_05@mail.ru>
2025-01-27 17:09:12 +0300

    fix prac 3

--- practice/practise-3/src/main/java/studying/DemoApplication.java
+++ practice/practise-3/src/main/java/studying/DemoApplication.java
@@ -13,31 +13,7 @@ import studying.services.CustomerStorage;
 import studying.services.HseCarService;
 
 @SpringBootApplication
-public class DemoApplication { var carService = new CarService();
-        var customerStorage = new CustomerStorage();
-        var hseCarService = new HseCarService(carService,customerStorage);
-        var pedalCarFactory = new PedalCarFactory();
-        var flyingCarFactory = new FlyingCarFactory();
-        var handCarFactory = new HandCarFactory();
-
-        customerStorage.addCustomer(new Customer("a", 6, 4, 200));
-        customerStorage.addCustomer(new Customer("b", 4, 6, 1));
-        customerStorage.addCustomer(new Customer("c", 6, 6, 52));
-        customerStorage.addCustomer(new Customer("d", 4, 4, 1000));
-
-        carService.addCar(pedalCarFactory, new PedalEngineParams(4));
-        carService.addCar(pedalCarFactory, new PedalEngineParams(7));
-        carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
-        carService.addCar(flyingCarFactory, EmptyEngineParams.DEFAULT);
-        carService.addCar(handCarFactory, EmptyEngineParams.DEFAULT);
-
-        System.out.println(customerStorage.toString());
-
-        hseCarService.sellCars();
-
-        for(var customer : customerStorage.getCustomers()) {
-            System.out.println(customer.toString());
-        }
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		var carService = new CarService();
